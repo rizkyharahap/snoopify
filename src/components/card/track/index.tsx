6f7091d5553d04da7a31c09999ce7cd76476d7fc
rizkyharahap <rizkyhh1999@gmail.com>
2022-04-22 10:42:25 +0700

    Add paggination search

--- src/components/card/track/index.tsx
+++ src/components/card/track/index.tsx
@@ -1,46 +1,54 @@
-import { Grid, Group, Image, Paper } from '@mantine/core';
+import { Divider, Grid, Group, Image, Paper } from '@mantine/core';
 import Subtitle from 'components/typography/subtitle';
 import Title from 'components/typography/title';
 import { memo, PropsWithChildren } from 'react';
 
 export interface TrackCardProps {
-  id?: string;
-  title?: string;
-  description?: string;
+  id: string;
+  title: string;
+  artist: string;
+  year: string | number;
   imageUrl?: string;
 }
 
 const TrackCard = ({
-  title = 'Mantap Jiwa',
-  description = 'Eitaro, IDOCS (2020)',
-  imageUrl = 'http://placehold.jp/d91775/ffffff/150x150.png',
+  title,
+  artist,
+  year,
+  imageUrl,
   children,
 }: PropsWithChildren<TrackCardProps>) => {
   return (
-    <Group position='apart' noWrap>
-      <Grid align='center' gutter='sm'>
-        <Grid.Col span={3}>
-          <Paper shadow='xl' radius='sm'>
-            <Image
-              radius='sm'
-              width='100%'
-              height='100%'
-              style={{ minHeight: 44 }}
-              src={imageUrl}
-              alt={title}
-            />
-          </Paper>
-        </Grid.Col>
+    <div>
+      <Group position='apart' noWrap mr='xl'>
+        <Grid align='center' gutter='sm'>
+          <Grid.Col span={3}>
+            <Paper shadow='xl' radius='sm'>
+              <Image
+                radius='sm'
+                width='100%'
+                height='100%'
+                style={{ minHeight: 44 }}
+                src={imageUrl}
+                alt={title}
+              />
+            </Paper>
+          </Grid.Col>
 
-        <Grid.Col span={9}>
-          <Title>{title}</Title>
+          <Grid.Col span={9}>
+            <Title>{title}</Title>
 
-          {description && <Subtitle lineClamp={1}>{description}</Subtitle>}
-        </Grid.Col>
-      </Grid>
+            <Subtitle lineClamp={1}>
+              {artist} &bull; {year}
+            </Subtitle>
+          </Grid.Col>
+        </Grid>
 
-      <Group noWrap>{children}</Group>
-    </Group>
+        <Group noWrap>{children}</Group>
+      </Group>
+
+      <Divider mt='lg' />
+    </div>
   );
 };
 
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,15 @@
 import {
   Container,
   LoadingOverlay,
+  Pagination,
   SimpleGrid,
+  Space,
   useMantineTheme,
 } from '@mantine/core';
 import { skipToken } from '@reduxjs/toolkit/dist/query';
 import TrackCard from 'components/card/track';
 import Subtitle from 'components/typography/subtitle';
+import { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useGetSearchTracksQuery } from 'services/api/searchApi';
 import { milisToMinutesAndSeconds } from 'services/helpers/convertTime';
@@ -14,14 +17,19 @@ import { useResponseHandlerQuery } from 'services/hooks/useResponseHandlerQuery'
 import SelectTrackAction from './components/action';
 
 const SearchPage = () => {
+  const [activePage, setPage] = useState(1);
+
   const theme = useMantineTheme();
   const [params] = useSearchParams();
 
   const query = params.get('q');
   const { data, isLoading, isFetching, isSuccess, isError, error } =
-    useGetSearchTracksQuery(query ? { q: query } : skipToken, {
-      refetchOnMountOrArgChange: true,
-    });
+    useGetSearchTracksQuery(
+      query ? { q: query, offset: activePage } : skipToken,
+      {
+        refetchOnMountOrArgChange: true,
+      },
+    );
 
   useResponseHandlerQuery({
     error,
@@ -30,7 +38,7 @@ const SearchPage = () => {
   });
 
   return (
-    <Container px='xs' size='lg'>
+    <Container px='xs' size='md'>
       <LoadingOverlay
         visible={isLoading || isFetching}
         loaderProps={{ size: 'xl', color: 'white', variant: 'bars' }}
@@ -39,34 +47,45 @@ const SearchPage = () => {
       />
 
       <SimpleGrid
-        spacing='md'
+        spacing='lg'
         cols={1}
-        breakpoints={[
-          { minWidth: theme.breakpoints.lg, cols: 2 },
-          { minWidth: theme.breakpoints.xl, cols: 3 },
-        ]}
+        breakpoints={[{ minWidth: theme.breakpoints.md, cols: 2 }]}
       >
         {data?.items.map(track => {
           const { id, artists, name, duration_ms, album } = track;
 
           return (
-            <TrackCard
-              key={id}
-              id={id}
-              title={name}
-              description={`${artists[0].name} &bull; (${new Date(duration_ms)
-                .toISOString()
-                .substring(14, 5)})`}
-              imageUrl={album.images[0].url}
-            >
-              <SelectTrackAction track={track} />
-              <Subtitle size='md'>
-                {milisToMinutesAndSeconds(duration_ms)}
-              </Subtitle>
-            </TrackCard>
+            <>
+              <TrackCard
+                key={id}
+                id={id}
+                title={name}
+                artist={artists[0].name}
+                year={new Date(album?.release_date).getFullYear()}
+                imageUrl={album.images[0].url}
+              >
+                <SelectTrackAction track={track} />
+                <Subtitle size='md'>
+                  {milisToMinutesAndSeconds(duration_ms)}
+                </Subtitle>
+              </TrackCard>
+            </>
           );
         })}
       </SimpleGrid>
+
+      <Space h='xl' />
+
+      {data?.total && data.total > 1 && (
+        <Pagination
+          color='brand'
+          ml='auto'
+          align='flex-end'
+          total={data.total}
+          page={activePage}
+          onChange={setPage}
+        />
+      )}
     </Container>
   );
 };
